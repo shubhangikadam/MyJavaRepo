676d258bc05c805eb47c36ae44d3976917332d74
Shubhangi Kadam <shubhangi_kadam@persistent.co.in>
2018-07-26 15:12:10 +0530

    Display Date function is called using object

--- HelloWordGit.java
+++ HelloWordGit.java
@@ -13,5 +13,9 @@ public viod displayDate()
 
 public static void main(String[] args){
     System.out.println("Hellow World -------Welcome to git");
-    }
+    
+    HellowWord w1=new Hellow();
+    w1.displayDate();
+
+}
 }
