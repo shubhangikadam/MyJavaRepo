b8563536361295b0363f3bb07da3235c75547b24
Shubhangi Kadam <shubhangi_kadam@persistent.co.in>
2018-07-26 14:53:32 +0530

    Display Date method addede

--- HelloWordGit.java
+++ HelloWordGit.java
@@ -1,6 +1,11 @@
 cass HellowOrd
+
 {
-    public static void main(String[] args){
+    
+
+public viod displayDate();
+
+public static void main(String[] args){
     System.out.println("Hellow World -------Welcome to git");
     }
 }
