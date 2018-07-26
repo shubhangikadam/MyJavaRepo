d22b7894cd5e905c0a5ecc0481c994b2b5daa47d
Shubhangi Kadam <shubhangi_kadam@persistent.co.in>
2018-07-26 14:55:52 +0530

    in display Date

--- HelloWordGit.java
+++ HelloWordGit.java
@@ -3,7 +3,13 @@ cass HellowOrd
 {
     
 
-public viod displayDate();
+public viod displayDate()
+{
+
+   System.out.println("Display todays 123");
+
+}
+
 
 public static void main(String[] args){
     System.out.println("Hellow World -------Welcome to git");
